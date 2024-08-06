567e688ad4446ca5158b0bffe414fdaf01c0a1b3
jae <ahmadjaelani8685@gmail.com>
2024-08-06 10:22:36 +0700

    Minor update

--- larest-user/src/App.jsx
+++ larest-user/src/App.jsx
@@ -5,51 +5,51 @@ import ContextProvider from "./context/ContextProvider";
 import TopProgressBar from "./components/Elements/Indicator/TopProgressBar";
 
 const baseTheme = extendTheme({
-    colorSchemes: {
-        light: {
-            palette: {
-                primary: {
-                    solidColor: '#ffffff',
-                    solidBg: '#f97316', // orange bg-500
-                    solidHoverBg: '#f97316', // orange bg-600
-                    solidDisabledBg: 'var(--disabled)',
-                    solidActiveBg: '#f97316', // orange bg-600
-                    outlinedColor: '#f97316', // orange bg-600
-                    outlinedBorder: '#f97316', // orange bg-500
-                    outlinedHoverBorder: undefined,
-                    outlinedHoverBg: '#fffffff', // orange bg-100
-                    outlinedActiveBg: '#fffffff', // orange bg-200
-                    loadingColor: '#f97316',
-                },
-                warning: {
-                    solidBg: '#eab308', // yellow bg-500
-                    solidHoverBg: '#ca8a04', // yellow bg-600
-                    solidActiveBg: '#ca8a04', // yellow bg-600
-                    outlinedColor: '#ca8a04', // yellow bg-600
-                    outlinedBorder: '#eab308', // yellow bg-500
-                    outlinedHoverBorder: undefined,
-                    outlinedHoverBg: '#fef3c7',  // yellow bg-100
-                    outlinedActiveBg: '#fef08a', // yellow bg-200
-                },
-                dark: {
-                    solidColor: '#ffffff',
-                    solidBg: 'var(--dark)',
-                    solidHoverBg: 'var(--dark-hover)',
-                    solidActiveBg: 'var(--dark-hover)',
-                    solidDisabledBg: 'var(--disabled)',
-                },
-                focusVisible: 'rgba(66, 153, 225, 0.6)',
-            },
+  colorSchemes: {
+    light: {
+      palette: {
+        primary: {
+          solidColor: '#ffffff',
+          solidBg: '#f97316', // orange bg-500
+          solidHoverBg: '#f97316', // orange bg-600
+          solidDisabledBg: 'var(--disabled)',
+          solidActiveBg: '#f97316', // orange bg-600
+          outlinedColor: '#f97316', // orange bg-600
+          outlinedBorder: '#f97316', // orange bg-500
+          outlinedHoverBorder: undefined,
+          outlinedHoverBg: '#fffffff', // orange bg-100
+          outlinedActiveBg: '#fffffff', // orange bg-200
+          loadingColor: '#f97316',
         },
+        warning: {
+          solidBg: '#eab308', // yellow bg-500
+          solidHoverBg: '#ca8a04', // yellow bg-600
+          solidActiveBg: '#ca8a04', // yellow bg-600
+          outlinedColor: '#ca8a04', // yellow bg-600
+          outlinedBorder: '#eab308', // yellow bg-500
+          outlinedHoverBorder: undefined,
+          outlinedHoverBg: '#fef3c7',  // yellow bg-100
+          outlinedActiveBg: '#fef08a', // yellow bg-200
+        },
+        dark: {
+          solidColor: '#ffffff',
+          solidBg: 'var(--dark)',
+          solidHoverBg: 'var(--dark-hover)',
+          solidActiveBg: 'var(--dark-hover)',
+          solidDisabledBg: 'var(--disabled)',
+        },
+        focusVisible: 'rgba(66, 153, 225, 0.6)',
+      },
     },
+  },
 });
 
 export default function App() {
-    return (
-        <CssVarsProvider theme={baseTheme}>
-            <ContextProvider>
-                <RouterProvider fallbackElement={<TopProgressBar />} router={router} />
-            </ContextProvider>
-        </CssVarsProvider>
-    );
+  return (
+    <CssVarsProvider theme={baseTheme}>
+      <ContextProvider>
+        <RouterProvider fallbackElement={<TopProgressBar />} router={router} />
+      </ContextProvider>
+    </CssVarsProvider>
+  );
 }
