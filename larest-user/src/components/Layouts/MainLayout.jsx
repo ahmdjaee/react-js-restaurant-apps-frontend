2f6b6ca6dfe0d410ea7b099833b19bb756c1b2bc
jae <ahmadjaelani8685@gmail.com>
2024-08-06 15:01:18 +0700

    Minor Update

--- larest-user/src/components/Layouts/MainLayout.jsx
+++ larest-user/src/components/Layouts/MainLayout.jsx
@@ -17,21 +17,10 @@ export async function loader() {
   }
 }
 
-function setViewportHeight() {
-  const vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-}
-
 function MainLayout() {
   const { carts } = useLoaderData();
   const [__, _, user] = useFetchData('/users/current');
   const { setUser } = useStateContext();
-
-  useEffect(() => {
-    setViewportHeight();
-    window.addEventListener('resize', setViewportHeight);
-    return () => window.removeEventListener('resize', setViewportHeight);
-  }, []);
 
   useEffect(() => {
     setUser(user?.data)
@@ -41,7 +30,7 @@ function MainLayout() {
     <CartProvider>
       <TopProgressBar />
       <TopNavBar carts={carts} />
-      <div className=" overflow-y-auto" style={{height: 'calc(var(--vh, 1vh) * 100)'}}>
+      <div className="flex-1 overflow-y-auto">
         <Outlet />
       </div>
       <BottomNavBar />
